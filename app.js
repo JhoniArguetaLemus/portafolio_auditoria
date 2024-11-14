08241a18462a179f66a5fed50d1ab0b3e072f336
JhoniArguetaLemus <isnp111022@ugb.edu.sv>
2024-11-14 15:37:53 -0600

    primer commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const menu=document.getElementById('menu');
+const menu_opciones=document.getElementById('menu_opciones');
+
+const cilindro_menu=document.getElementById('cilindro_menu');
+
+
+
+
+menu.addEventListener('click', ()=>{
+     if(menu_opciones.classList.contains('visible')){
+        menu_opciones.classList.remove('visible');
+        cilindro_menu.src='Imagenes/menu.png';
+     }else{
+        menu_opciones.classList.add('visible');
+        cilindro_menu.src='Imagenes/close.png';
+
+     }
+})
