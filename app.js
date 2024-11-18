ed96edcbb19f761d6ce15be9aac2b682dc21dccf
JhoniArguetaLemus <isnp111022@ugb.edu.sv>
2024-11-18 16:48:00 -0600

    capitulo III

--- app.js
+++ app.js
@@ -18,3 +18,97 @@ menu.addEventListener('click', ()=>{
 })
 
 
+
+//capitulo III
+
+
+//carrousel
+
+const items = document.querySelectorAll('.carousel-item');
+        let currentIndex = 0;
+
+        // Función para mostrar un slide específico
+        function showSlide(index) {
+            items.forEach((item, i) => {
+                item.classList.remove('active');
+                if (i === index) {
+                    item.classList.add('active');
+                }
+            });
+        }
+
+        // Control de los botones "Anterior" y "Siguiente"
+        document.getElementById('prev').addEventListener('click', () => {
+            currentIndex = (currentIndex - 1 + items.length) % items.length;
+            showSlide(currentIndex);
+        });
+
+        document.getElementById('next').addEventListener('click', () => {
+            currentIndex = (currentIndex + 1) % items.length;
+            showSlide(currentIndex);
+        });
+
+        // Opcional: Auto-rotación del carrusel
+        setInterval(() => {
+            currentIndex = (currentIndex + 1) % items.length;
+            showSlide(currentIndex);
+        }, 5000);
+
+//mostrar pdf
+
+const button = document.getElementById('mostrar_pdf1');
+const pdfContainer = document.getElementById('divFrame1');
+      
+      button.addEventListener('click', ()=>{
+ 
+         mostrarPDF(pdfContainer)
+
+      })
+
+
+const button2 = document.getElementById('mostrar_pdf2');
+const pdfContainer2 = document.getElementById('divFrame2');
+
+button2.addEventListener('click', ()=>{
+   mostrarPDF(pdfContainer2);
+   
+})
+
+const button3 = document.getElementById('mostrar_pdf3');
+const pdfContainer3 = document.getElementById('divFrame3');
+
+button3.addEventListener('click', ()=>{
+   mostrarPDF(pdfContainer3);
+})
+
+function mostrarPDF(pdfContenedor){
+   
+   if (pdfContenedor.style.display === 'none' || pdfContenedor.style.display === '') {
+      pdfContenedor.style.display = 'flex'; // Muestra el contenedor
+  } else {
+     pdfContenedor.style.display = 'none'; // Oculta el contenedor
+  }
+
+
+}
+
+
+
+//acordion
+
+function toggleAccordion1(id) {
+   const content = document.getElementById(id);
+   const icon = document.getElementById(`icon-${id}`);
+
+   content.classList.toggle('hidden');
+   icon.classList.toggle('rotate-180');
+}
+
+
+function toggleAccordion2(id) {
+   const content = document.getElementById(id);
+   const icon = document.getElementById(`icon-${id}`);
+
+   content.classList.toggle('hidden');
+   icon.classList.toggle('rotate-180');
+}
